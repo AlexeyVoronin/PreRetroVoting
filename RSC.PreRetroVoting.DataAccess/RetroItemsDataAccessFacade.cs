a61af6b32e35284ae76ac17d6853bf51290834af
sashar2 <sashar2@yandex.ru>
2013-04-11 19:55:26 +0400

    Хранилище перемещено в temp directory.

--- RSC.PreRetroVoting.DataAccess/RetroItemsDataAccessFacade.cs
+++ RSC.PreRetroVoting.DataAccess/RetroItemsDataAccessFacade.cs
@@ -5,18 +5,21 @@ using System.Text;
 using RSC.PreRetroVoting.DataAccess.RetroItemsAdders;
 using RSC.PreRetroVoting.DataAccess.Xml;
 using RSC.PreRetroVoting.DataAccess.FileSystem;
+using System.IO;
 
 namespace RSC.PreRetroVoting.DataAccess
 {
   public sealed class RetroItemsDataAccessFacade : IRetroItemsDataAccessFacade
   {
     public RetroItemsDataAccessFacade()
-    {      
+    {
+      var rootDirectory = Path.GetTempPath();
+      var retroItemsXmlFilePath = Path.Combine(rootDirectory, RetroItemXmlFile.FileName);
       var xmlFileProvider = new TemplateXmlFileProviderDecorator(
-          new XDocumentFileProvider(RetroItemXmlFile.FileName),
+          new XDocumentFileProvider(retroItemsXmlFilePath),
           new FileOperations(),
           RetroItemXmlFile.Template,
-          RetroItemXmlFile.FileName);
+          retroItemsXmlFilePath);
       Adder = new FileBasedRetroItemAdder(xmlFileProvider);
       Provider = new FileBasedRetroItemsProvider(xmlFileProvider);
     }
